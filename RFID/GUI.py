3fb6379c999213c95001900933d11607ff3cbfec
albert752 <azemar.albert@gmail.com>
2018-10-21 21:07:13 +0200

    Added the cross icon and switch functionality to it. Also some padding for the UID to avoid resize of teh window

--- RFID/GUI.py
+++ RFID/GUI.py
@@ -5,6 +5,12 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import GLib, Gtk, GObject, GdkPixbuf
 WORKINGDIR = os.getcwd()
 
+tickPath = os.path.join(WORKINGDIR, "data", "icons", "tick.png")
+crossPath = os.path.join(WORKINGDIR, "data", "icons", "cross.png")
+defaultText = "Please identify yourself"
+
+
+
 class LogInWindow(Gtk.Window):
 
     def __init__(self):
@@ -20,7 +26,7 @@ class LogInWindow(Gtk.Window):
         self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
         self.hbox.pack_start(self.vbox, True, True, 10)
 
-        self.label = Gtk.Label(label="Please identify yourself", justify=Gtk.Justification.LEFT)
+        self.label = Gtk.Label(label=defaultText, justify=Gtk.Justification.LEFT)
         self.vbox.pack_start(self.label, True, True, 10)
 
         self.button = Gtk.Button(label="Clear")
@@ -29,19 +35,22 @@ class LogInWindow(Gtk.Window):
 
         self.image = Gtk.Image()
 
-        self.loader = GdkPixbuf.Pixbuf.new_from_file_at_size(os.path.join(WORKINGDIR, "data", "icons", "tick.png"), 40, 40)
-        self.image.set_from_pixbuf(self.loader)
-        self.hbox.pack_start(self.image, True, True, 10)
+        self.tick = GdkPixbuf.Pixbuf.new_from_file_at_size(tickPath, 40, 40)
+        self.cross = GdkPixbuf.Pixbuf.new_from_file_at_size(crossPath, 40, 40)
 
+        self.image.set_from_pixbuf(self.cross)
+        self.hbox.pack_start(self.image, True, True, 10)
         self.show_all()
 
         self.lector.start(self.onUID)
 
     def onUID(self, UID):
-        GLib.idle_add(self.label.set_text, UID)
+        GLib.idle_add(self.label.set_text, UID.center(len(defaultText)))
+        GLib.idle_add(self.image.set_from_pixbuf, self.tick)
 
     def on_button_clicked(self, widget):
         self.label.set_text("Please identify yourself")
+        self.image.set_from_pixbuf(self.cross)
 
 
 if __name__ == '__main__':
