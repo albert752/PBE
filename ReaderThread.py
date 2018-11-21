e0f46499d33666c569e80177e802e041a8e5080e
Mefiso <polpegra@gmail.com>
2018-11-21 16:13:22 +0100

    Add files via upload

---
+++ ReaderThread.py
@@ -0,0 +1,17 @@
+import threading
+import time
+from RFID1 import RFID
+from gi.repository import GLib
+
+class ReaderThread:
+	def __init__(self, isTest=False, handler):
+		self.reader=RFID()
+		self.handler=handler
+	def start(self):
+		self.thread=threading.Thread(target=self.readUID, args=[self.handler])
+		self.thread.daemon=True
+		self.thread.start()
+
+	def readUID(self, handler):
+		GLib.idle_add(handler, self.reader.read())
+
